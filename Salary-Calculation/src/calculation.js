1c12ca82cc0aa5528b2082797fd9dc727afc2918
renan12333 <49961304+renan12333@users.noreply.github.com>
2019-07-25 19:19:54 -0300

    Add files via upload

--- Salary-Calculation/src/calculation.js
+++ Salary-Calculation/src/calculation.js
@@ -3,26 +3,54 @@ let data = {
     hours : document.getElementById("hours"),
     price : document.getElementById("price"),
     dependents : document.getElementById("dependents"),
-    INSS : document.getElementById("INSS"),
-    IRRF : document.getElementById("IRRF"),
 }
-
-
+  
+function check(){
+    if(true){
+        calculate()
+    }
+    else{
+        alert('Fill in the fields correctly')
+    }
+}
 
 function calculate(){
     var grossSalary = (data.hours.value * data.price.value)
-    var netSalary = 0
-    var valueINSS = 0
-    var valueIRRF = (grossSalary - valueINSS)
+    var INSS = ""
+    valueINSS = 0
 
-    let text = `<p>Gross salary ${grossSalary}</p>`
-    text += `<p>Net salary ${netSalary} </p>`
-    text +=`<p>INSS value ${valueINSS} </p>`
-    text += `<p>IRRF value ${valueIRRF} </p>`
+    if(grossSalary <= 1317.08){
+        valueINSS = (grossSalary * 8/100)
+        INSS = '8%'
+    }
+    else if (grossSalary <= 2195.12) {
+        valueINSS = (grossSalary * 9/100)
+        INSS = '9%'
+    }
+    else if (grossSalary <= 4390.24) {
+        valueINSS = (grossSalary * 11/100)
+        INSS = '11%'
+    }
+    else if (grossSalary >= 4390.25) {
+        valueINSS = (grossSalary + 482.93)
+        INSS = '482,93'
+    }
+
+    var valueIRRF = (grossSalary - valueINSS)
+    var netSalary = (grossSalary - (valueIRRF + valueINSS))
+    
+    let text =`INSS value ${valueINSS}, INSS percentage ${INSS} <br>`
+    text += `IRRF value ${valueIRRF}, IRRF percentage <br>` 
+    text += `Gross salary ${grossSalary}<br>`
+    text += `Net salary ${netSalary}`
     
     document.getElementById("answer").innerHTML = text
 }
 
-calculate()
+document.addEventListener('keydown', event => {
+    if(event.keyCode === 13) {
+        check()
+    }
+})
 
 //https://www.google.com/search?q=IRRF&client=ms-android-motorola&prmd=niv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwil5YSmvs7jAhVyw1kKHemcAuIQ_AUoAnoECBIQAg&biw=360&bih=512#imgrc=kbZJghbLa2ekmM
